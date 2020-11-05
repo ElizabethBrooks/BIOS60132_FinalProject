391a60934191e3acfb95fbaa38bb7fd77a8c7774
Mae <merimaat1@gmail.com>
2020-11-04 21:55:52 -0500

    Added genomic vs rna analysis

--- finalProject.sh
+++ finalProject.sh
@@ -9,10 +9,10 @@ module load julia/1.4.1
 #Set inputs
 merSize=16
 sketchSize=400
-inputNames="speciesList.csv"
-outputDist="distanceMatrix.csv"
-outputPhylo="treeNetwork.txt"
-outputTopo="treeTopology.txt"
+inputNames="genomicList.csv"
+outputDist="../Data/Results_FinalProject/distanceMatrix_genomic.csv"
+outputPhylo="../Data/Results_FinalProject/treeNetwork_genomic.txt"
+outputTopo="../Data/Results_FinalProject/treeTopology_genomic.txt"
 
 #Run julia script
 julia ./sequenceDistance_phylogeny.jl $merSize $sketchSize $inputNames $outputDist $outputPhylo $outputTopo
